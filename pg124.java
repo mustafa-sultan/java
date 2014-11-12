ea0f38578bd0868cf0a09e0b9385d59dd524483b
Mustafa Alaa <mustafaalaa@users.noreply.github.com>
2014-11-12 15:54:47 +0000

    Update pg124.java

--- pg124.java
+++ pg124.java
@@ -1,3 +1,29 @@
 // Java Program that counts down to ten using if and for statements.
 // Written By: Mustafa Alaa
 // Date: 12/11/2014
+// Java Program to input ten numbers from the keyboard and then print the sum.
+// Written By: Mustafa Alaa
+// Date: 12/11/2014
+public class pg124
+{
+  public static void main (string [] args) throws InterrputException
+  {
+    System.out.println("Commencing Countdown");
+    
+    for (i = 10; i <= 0; i--)
+    {
+      System.out.println(i);
+      
+      if (i == 3)
+      {
+        System.out.println("Ignition")
+      }
+      
+      if (i == 1)
+      {
+        System.out.println("Blast Off")
+      }
+      
+    }
+  } // end main
+} // end class pg124
