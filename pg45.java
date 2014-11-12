3cae8b80b1e816bea926f6366a59222d154788e1
Mustafa Alaa <mustafaalaa@users.noreply.github.com>
2014-11-12 15:29:25 +0000

    Update pg45.java

--- pg45.java
+++ pg45.java
@@ -1,1 +1,19 @@
-public class 
+public class pg45
+{
+  public static void main (Strin[] args)
+  {
+    Scanner = input = new Scanner (System.in); // scanner
+    
+    int i; // for loop variable
+    sum = 0; // total variable
+    int current_input; // for loop input variable
+    
+    for (i = 1; i <= 10; i++)
+    {
+      System.out.println("Please enter number" + i  + "and press return: ");
+      current_input = input.nextInt();
+      sum += current_input
+    }
+     System.out.println("sum = " + sum);
+  } // end main
+} // end clas pg45
